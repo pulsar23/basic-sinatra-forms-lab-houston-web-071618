9996b0a722e1dee695e318668ba4c849c65d189d
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-08-07 17:39:56 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+post '/team' do
+  params.to_s
+  erb :team
+end
+
+  # get '/team' do
+  #   erb :team
+  # end
 
 end
